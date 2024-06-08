db32567d5281f59f8c4cf9efeb5662a23507162b
rsoylu <rana@soylu.org>
2024-06-07 20:34:06 -0500

    fix mistake

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,23 +1,16 @@
-import Title from "./Title";
 import { services } from "../data";
+import Title from "./Title";
+import Service from "./Service";
 const Services = () => {
   return (
     <section className="section services" id="services">
       <Title title="our" subtitle="services" />
-      {services.map((service) => {
-        const { id, icon, title, text } = service;
-        return (
-          <article className="service" key={id}>
-            <span className="service-icon">
-              <i className={icon}></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">{title}</h4>
-              <p className="service-text">{text}</p>
-            </div>
-          </article>
-        );
-      })}
+
+      <div className="section-center services-center">
+        {services.map((service) => {
+          return <Service {...service} key={service.id} />;
+        })}
+      </div>
     </section>
   );
 };
