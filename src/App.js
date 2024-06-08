349099c9fc899d9108f9d699a5576c878a5a8889
rsoylu <rana@soylu.org>
2024-06-07 19:46:44 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Hero from "./components/Hero.js";
+import About from "./components/About.js";
+import Services from "./components/Services.js";
+import Tours from "./components/Tours.js";
+import Footer from "./components/Footer.js";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Services.js
@@ -0,0 +1,24 @@
+import Title from "./Title";
+import { services } from "../data";
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subtitle="services" />
+      {services.map((service) => {
+        const { id, icon, title, text } = service;
+        return (
+          <article className="service" key={id}>
+            <span className="service-icon">
+              <i className={icon}></i>
+            </span>
+            <div className="service-info">
+              <h4 className="service-title">{title}</h4>
+              <p className="service-text">{text}</p>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+export default Services;
